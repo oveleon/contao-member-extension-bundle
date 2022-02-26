914938c6af0e60a8daf32329f217e41a4333de67
Sebastian Zoglowek <sebastian@oveleon.de>
2022-02-26 03:06:50 +0100

    [Update] Added noAvatar to ModuleAvatar

--- src/Resources/contao/modules/ModuleAvatar.php
+++ src/Resources/contao/modules/ModuleAvatar.php
@@ -1,105 +1,119 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of Oveleon ContaoMemberExtension Bundle.
  *
- * (c) https://www.oveleon.de/
+ * @package     contao-member-extension-bundle
+ * @license     MIT
+ * @author      Daniele Sciannimanica   <https://github.com/doishub>
+ * @author      Fabian Ekert            <https://github.com/eki89>
+ * @author      Sebastian Zoglowek      <https://github.com/zoglo>
+ * @copyright   Oveleon                 <https://www.oveleon.de/>
  */
 
 namespace Oveleon\ContaoMemberExtensionBundle;
 
-use Patchwork\Utf8;
+use Contao\BackendTemplate;
+use Contao\FilesModel;
+use Contao\FrontendUser;
+use Contao\MemberModel;
+use Contao\Module;
+use Contao\System;
 
 /**
  * Class ModuleAvatar
  *
  * @author Fabian Ekert <fabian@oveleon.de>
+ * @author Sebastian Zoglowek <https://github.com/zoglo>
  */
-class ModuleAvatar extends \Module
+class ModuleAvatar extends Module
 {
+    /**
+     * Template.
+     *
+     * @var string
+     */
+    protected $strTemplate = 'member_avatar';
 
-	/**
-	 * Template
-	 * @var string
-	 */
-	protected $strTemplate = 'member_avatar';
+    /**
+     * Default avatar file path
+     *
+     * @var string
+     */
+    private static $strDefaultPath = 'bundles/contaomemberextension/avatar.png';
 
-	/**
-	 * Return a wildcard in the back end
-	 *
-	 * @return string
-	 */
-	public function generate()
-	{
-		if (TL_MODE == 'BE')
-		{
-			/** @var BackendTemplate|object $objTemplate */
-			$objTemplate = new \BackendTemplate('be_wildcard');
+    /**
+     * Return a wildcard in the back end
+     *
+     * @return string
+     */
+    public function generate()
+    {
+        $request = System::getContainer()->get('request_stack')->getCurrentRequest();
 
-			$objTemplate->wildcard = '### ' . Utf8::strtoupper($GLOBALS['TL_LANG']['FMD']['avatar'][0]) . ' ###';
-			$objTemplate->title = $this->headline;
-			$objTemplate->id = $this->id;
-			$objTemplate->link = $this->name;
-			$objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
+        if ($request && System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
+        {
+            $objTemplate = new BackendTemplate('be_wildcard');
+            $objTemplate->wildcard = '### ' . mb_strtoupper($GLOBALS['TL_LANG']['FMD']['avatar'][0], 'UTF-8') . ' ###';
+            $objTemplate->title = $this->headline;
+            $objTemplate->id = $this->id;
+            $objTemplate->link = $this->name;
+            $objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
 
-			return $objTemplate->parse();
-		}
+            return $objTemplate->parse();
+        }
 
-		// Return if user is not logged in
-		if (!FE_USER_LOGGED_IN)
-		{
-			return '';
-		}
+        // Return if user is not logged in
+        $tokenChecker = System::getContainer()->get('contao.security.token_checker');
+        $blnFeUserLoggedIn = $tokenChecker->hasFrontendUser();
 
-		if ($this->memberTpl != '')
-		{
-			$this->strTemplate = $this->memberTpl;
-		}
-
-		return parent::generate();
-	}
-
-	/**
-	 * Generate the module
-	 */
-	protected function compile()
-	{
-        $this->size = $this->imgSize;
-
-        $this->import('FrontendUser', 'User');
-
-        if ($this->User->avatar == '' && \Config::get('defaultAvatar') == '')
+        if (!$blnFeUserLoggedIn)
         {
             return '';
         }
 
-        if ($this->User->avatar == '')
-        {
-            $objFile = \FilesModel::findByUuid(\Config::get('defaultAvatar'));
+        $this->strTemplate = $this->memberTpl ?: 'member_avatar';
 
-            if ($objFile === null || !is_file(TL_ROOT . '/' . $objFile->path))
+        return parent::generate();
+    }
+
+    /**
+     * Generate the module
+     */
+    protected function compile()
+    {
+        $objTemplate = $this->Template;
+
+        $this->size = $this->imgSize;
+
+        $objTemplate->noAvatar = true;
+
+        $this->import(FrontendUser::class, 'User');
+        $objMember = MemberModel::findByPk($this->User->id);
+
+        $projectDir = System::getContainer()->getParameter('kernel.project_dir');
+
+
+        if (!$this->User->avatar)
+        {
+            $objTemplate->singleSRC = self::$strDefaultPath;
+        }
+        else
+        {
+            $objFile = FilesModel::findByUuid($this->User->avatar);
+
+            if ($objFile === null || !is_file($projectDir . '/' . $objFile->path))
             {
-                return '';
+                $objTemplate->singleSRC = self::$strDefaultPath;
             }
-
-            $this->singleSRC = $objFile->path;
-
-            $this->addImageToTemplate($this->Template, $this->arrData);
-            return;
+            else
+            {
+                $objTemplate->noAvatar = false;
+                $this->singleSRC = $objFile->path;
+                $this->addImageToTemplate($this->Template, $this->arrData);
+            }
         }
-
-        $objFile = \FilesModel::findByUuid($this->User->avatar);
-
-        if ($objFile === null || !is_file(TL_ROOT . '/' . $objFile->path))
-        {
-            $this->singleSRC = \FilesModel::findByUuid(\Config::get('defaultAvatar'))->path;
-
-            $this->addImageToTemplate($this->Template, $this->arrData);
-            return;
-        }
-
-        $this->singleSRC = $objFile->path;
-
-        $this->addImageToTemplate($this->Template, $this->arrData, null, null, $objFile);
-	}
+    }
 }
